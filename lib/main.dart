62fb23d4fc39c09e3f9f64315a2c7639a2f21db9
inf3900 <inf3900@hs-worms.de>
2023-10-28 13:08:59 +0200

    Everything works again. Next is the Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,12 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(title: '',));
 }
 class MyApp extends StatefulWidget {
-  const MyApp({ super.key });
+  const MyApp({ super.key, required this.title });
+
+  final String title;
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -22,39 +24,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isSwitched = false;
-
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context)  => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: isSwitched ? ThemeClass.darkTheme : ThemeClass.lightTheme,
-        home:  Scaffold(
-          appBar: AppBar(title: const Text('The Ultimate Switch'), actions: [
-           Switch(
-             activeColor: Colors.greenAccent,
-             value: isSwitched,
-             onChanged: (value){
-                setState((){
-                  isSwitched = !isSwitched;
-                });
-             })
-        ]),
-    ),
-  );
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   double _doubleValue = 0.0;
   String _printableValue = '0.0';
@@ -79,14 +48,23 @@ class _MyHomePageState extends State<MyHomePage> {
       _printableValue = _doubleValue.toStringAsFixed(1);
     });
   }
-
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+  Widget build(BuildContext context)  => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: isSwitched ? ThemeClass.darkTheme : ThemeClass.lightTheme,
+        home:  Scaffold(
+          appBar: AppBar(title: const Text('The Ultimate Switch'), actions: [
+           Switch(
+             activeColor: Colors.greenAccent,
+             value: isSwitched,
+             onChanged: (value){
+                setState((){
+                  isSwitched = !isSwitched;
+                });
+             })
+        ]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -99,14 +77,14 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             const Text(
-              'The Current Temperature is '
+                'The Current Temperature is '
             ),
             Text(
               _preSymbol + _printableValue,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             const Text(
-              '° C'
+                '° C'
             )
           ],
         ),
@@ -116,6 +94,81 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+    ),
+  );
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//   double _doubleValue = 0.0;
+//   String _printableValue = '0.0';
+//   String _preSymbol = '+';
+//
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//       //external factory Random([int? seed]);
+//       var boolValue = Random().nextBool();
+//       if(boolValue == true){
+//         _preSymbol ='+';
+//       }else{
+//         _preSymbol ='-';
+//       }
+//       _doubleValue = Random().nextDouble() * 6;
+//       _printableValue = _doubleValue.toStringAsFixed(1);
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//             const Text(
+//               'The Current Temperature is '
+//             ),
+//             Text(
+//               _preSymbol + _printableValue,
+//               style: Theme.of(context).textTheme.headlineMedium,
+//             ),
+//             const Text(
+//               '° C'
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
