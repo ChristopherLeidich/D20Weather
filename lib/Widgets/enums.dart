d7bd0396dc4ce5e6b62da1e7ceb027b4be351ae6
christopher <inf3900@hs-worms.de>
2023-11-15 23:56:21 +0100

    ...

--- lib/Widgets/enums.dart
+++ lib/Widgets/enums.dart
@@ -10,20 +10,28 @@ enum Picture {
   picture6,
 }
 
+String gebiet = '';
+
 extension PictureExtension on Picture {
   String get assetPath {
     switch (this) {
       case Picture.picture1:
+        gebiet ='Gletscher';
         return 'assets/images/glacier.jpg';
       case Picture.picture2:
+        gebiet ='Final Coil of Dalamut';
         return 'assets/images/KT3A7OD.jpeg';
       case Picture.picture3:
+        gebiet ='Wüste';
         return 'assets/images/9502ac62b81f208465c7beb0d4338c77.jpg';
       case Picture.picture4:
+        gebiet ='Südlicher Markt';
         return 'assets/images/44.jpg';
       case Picture.picture5:
+        gebiet ='Küste';
         return 'assets/images/1123010.jpg';
       case Picture.picture6:
+        gebiet ='Wald';
         return 'assets/images/hcXuUBG_1.jpg';
       default:
         throw ArgumentError('Invalid picture');
